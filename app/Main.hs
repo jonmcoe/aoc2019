d05a35e20fb2efcb44ef5c007790b94e51e8adc4
Jon Coe <jonmcoe@gmail.com>
2019-12-04 20:59:52 -0500

    add p1. performed in Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,29 @@
-module Main where
+module Main (gasCostCompounded, gasCost, main) where
 
 import Lib
 
+readInt :: String -> Integer
+readInt = read
+
+gasCost :: Integer -> Integer
+gasCost n = div n 3 - 2
+
+gasCostCompounded :: Integer -> Integer
+gasCostCompounded n
+  | n < 9     = 0
+  | otherwise = x + gasCostCompounded x where x = gasCost n -- what would be a recursion scheme?
+
+allGasCost :: [Integer] -> Integer
+allGasCost = sum . map gasCost
+
+allGasCostCompounded :: [Integer] -> Integer
+allGasCostCompounded = sum . map gasCostCompounded
+
 main :: IO ()
-main = someFunc
+main = do
+  text <- readFile "/Users/jon/repos/aoc2019/data/p01"
+  let
+    x = map readInt $ lines text
+    p1 = allGasCost x
+    p2 = allGasCostCompounded x
+  print [p1, p2]
