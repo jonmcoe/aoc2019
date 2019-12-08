20c7f1b4a97a04deb8082a06946a8dce24d3609c
Jon Coe <jonmcoe@gmail.com>
2019-12-08 12:00:01 -0500

    p4 revision

--- src/Days/Day04.hs
+++ src/Days/Day04.hs
@@ -1,10 +1,9 @@
 module Days.Day04 where
 
 import Data.Char
-import qualified Data.Map.Strict as M
 import Data.List.Split
+import Data.List (group)
 
-toDigits = map digitToInt . show
 parse = map (\x -> read  x :: Int) . splitOn "-"
 
 nonDescending :: Ord a => [a] -> Bool
@@ -12,11 +11,10 @@ nonDescending l = case l of
   x:y:rest -> x <= y && nonDescending (y:rest)
   _  -> True
 
-frequencies = foldl (\tally incoming -> if M.member incoming tally then M.adjust (+ 1) incoming tally else M.insert incoming 1 tally ) M.empty
-hasRepeat = any (< 1) . M.elems . frequencies
-hasTwopeat = elem 2 . M.elems . frequencies
+hasRepeat = any (> 1) . map length . group
+hasTwopeat = elem 2 . map length . group
 
-day04a t = show $ length $ filter (\x -> nonDescending x && hasRepeat x) (map toDigits [bottom..top])
+day04a t = show $ length $ filter (\x -> nonDescending x && hasRepeat x) (map show [bottom..top])
   where bottom:top:_ = parse t
-day04b t = show $ length $ filter (\x -> nonDescending x && hasTwopeat x) (map toDigits [bottom..top])
+day04b t = show $ length $ filter (\x -> nonDescending x && hasTwopeat x) (map show [bottom..top])
   where bottom:top:_ = parse t
