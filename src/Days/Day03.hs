5161ae6a13ff2cdbafb46a003ebb0654b66ca2bd
Jon Coe <jonmcoe@gmail.com>
2019-12-08 09:59:42 -0500

    p3 complete

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -2,26 +2,37 @@ module Days.Day03 where
 
 import Data.List.Split
 import qualified Data.Set as S
+import qualified Data.Map.Strict as M
 
-readInt :: String -> Integer
-readInt = read
+type PathEntry = (Int, Int)
 
 data Direction = Righty | Lefty | Up | Down deriving Show
 
-charToDirection :: Char -> Direction
-charToDirection 'R' = Righty
-charToDirection 'L' = Lefty
-charToDirection 'U' = Up
-charToDirection 'D' = Down
-charToDirection _ = error "can't parse"
-
---pathSet :: [(Direction, Int)] -> (Int, Int) -> S.Set[(Int, Int)] -> S.Set[(Int, Int)]
---pathSet [] _ _ = S.empty
---pathSet (d:ds) (x, y) soFar = S.union soFar () -- pattern match for drawing them all out by direction...
-
-parse = map (map r . splitOn ",") . lines
+pathEntryToDistance :: [(Direction, Int)] -> M.Map PathEntry Int
+pathEntryToDistance = fst . foldl f (M.empty, ((0,0),0))
   where
-    r s = (charToDirection (head s), readInt (tail s))
+    f (soFar, ((oldMostRecentX, oldMostRecentY), oldMostRecentMag)) incoming = (M.union soFar (M.fromList additions), newMostRecent)
+      where
+        newMostRecent = last additions
+        additions = case incoming of
+          (Righty, mag) -> [((oldMostRecentX + i, oldMostRecentY), oldMostRecentMag + i) | i <- [1..mag]]
+          (Lefty, mag)  -> [((oldMostRecentX - i, oldMostRecentY), oldMostRecentMag + i) | i <- [1..mag]]
+          (Up, mag)     -> [((oldMostRecentX, oldMostRecentY + i), oldMostRecentMag + i) | i <- [1..mag]]
+          (Down, mag)   -> [((oldMostRecentX, oldMostRecentY - i), oldMostRecentMag + i) | i <- [1..mag]]
 
-day03a = show . parse
-day03b = show . parse
+intersections :: String -> M.Map PathEntry Int
+intersections = foldl1 (M.intersectionWith (+)) . map pathEntryToDistance . parse
+  where
+    parse = map (map r . splitOn ",") . lines
+    r s = (charToDirection (head s), read (tail s)::Int)
+    charToDirection 'R' = Righty
+    charToDirection 'L' = Lefty
+    charToDirection 'U' = Up
+    charToDirection 'D' = Down
+    charToDirection _ = error "can't parse"
+
+manhattanDistance :: PathEntry -> Int
+manhattanDistance (x, y) = abs x + abs y
+
+day03a = show . minimum . map manhattanDistance . M.keys . intersections
+day03b = show . minimum . M.elems . intersections
