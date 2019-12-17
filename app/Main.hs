6e489f425adc5f0ec456358abda8d5d23ffb63f1
Jon Coe <jonmcoe@gmail.com>
2019-12-17 08:31:20 -0500

    change to putStrLn

--- app/Main.hs
+++ app/Main.hs
@@ -11,5 +11,5 @@ main = do
      (p1, p2) = daysMapping chosenDay
      dataFile = if length args > 1 then args!!1 else "./data/p" ++ chosenDay
    fullText <- readFile dataFile
-   print $ p1 fullText
-   print $ p2 fullText
+   putStrLn $ p1 fullText
+   putStrLn $ p2 fullText
