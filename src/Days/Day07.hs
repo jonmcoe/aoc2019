a296cb130642b22316b7e95021505f08cdf90393
Jon Coe <jonmcoe@gmail.com>
2019-12-21 15:44:01 -0500

    day07b complete

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -1,22 +1,46 @@
 module Days.Day07 where
 
 import Data.List (maximumBy, permutations)
-import Data.Ord (comparing)
+import Data.Maybe (fromJust)
+import qualified Data.Map.Strict as M
 
 import Days.Common
 
-getOutputForPair :: (Int, Int) -> String -> Int
-getOutputForPair (a,b) t = output $ compute $ newComputerState [a,b] t
+getOutputForPair :: (Int, Int) -> String -> Maybe Int
+getOutputForPair (a,b) = output . nextOutput . newComputerState [a,b]
 
 chainCompute :: String -> [Int] -> Int
-chainCompute t = foldl (\prevOutput ampSetting -> getOutputForPair (ampSetting, prevOutput) t) 0
+chainCompute t = foldl (\prevOutput ampSetting -> fromJust $ getOutputForPair (ampSetting, prevOutput) t) 0
+
+runFeedbackLoop :: String -> [Int] -> Int
+runFeedbackLoop t params = fromJust $ output $ (M.! 4) $ runFeedbackLoopImpl initialCsList 0
+  where
+    runFeedbackLoopImpl csList step = case output curResult of
+      Nothing  -> csList
+      Just csO -> runFeedbackLoopImpl
+                   (M.adjust
+                    (appendToInput csO)  -- append input to...
+                    (succ step `mod` 5)  -- ... the next step's machine
+                    (M.insert (step `mod` 5) curResult csList) -- update our just-run machine
+                   )
+                   (succ step)
+      where
+        curResult = nextOutput $ csList M.! (step `mod` 5)
+    initialCsList = M.fromList [ (0, newComputerState [head params, 0] t)
+                               , (1, newComputerState [params!!1] t)
+                               , (2, newComputerState [params!!2] t)
+                               , (3, newComputerState [params!!3] t)
+                               , (4, newComputerState [params!!4] t)
+                               ]
 
 day07a :: String -> String
-day07a t = show $ maximumBy (comparing snd) $ map (\x -> (x, chainCompute t x)) $ permutations [0..4]
+day07a = show . maximum . flip map (permutations [0..4]) . chainCompute
 
 
--- need to further generalize computer to yield FIRST output, not necessarily continue until halt command
--- i think we also need to maintain the same tape between runs
 day07b :: String -> String
-day07b t = show $ maximumBy (comparing snd) $ map (\x -> (x, chainCompute t x)) $ concatMap (generateRepetitions 1) (permutations [5..9])
-  where generateRepetitions n l = map (\x -> concat $ replicate x l) [1..n]
+day07b = show . maximum . flip map (permutations [5..9]) . runFeedbackLoop
+
+
+-- -- --
+--permRepetitons = concatMap (generateRepetitions 1) (permutations [5..9])
+--generateRepetitions n l = map (\x -> concat $ replicate x l) [1..n]
