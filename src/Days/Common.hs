9bbf5bddf675e02f9219f19900f1014fabf648ad
Jon Coe <jonmcoe@gmail.com>
2019-12-21 15:14:33 -0500

    isolate "step" function in computer

--- src/Days/Common.hs
+++ src/Days/Common.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE NamedFieldPuns #-}
 
-module Days.Common (ComputerState(tape, output), compute, newComputerState, parseComputerTape, newComputerStateParsedTape) where
+module Days.Common
+  (ComputerState(tape, output), compute, newComputerState, parseComputerTape, newComputerStateParsedTape, nextOutput, appendToInput) where
 
 import Data.List.Split
-
+import Data.Maybe (isJust)
 
 parseComputerTape t = map r $ splitOn "," t where r x = read x::Int
 
@@ -12,8 +13,11 @@ replaceAtIndexsValue l i v = concat [take (l!!i) l, [v], drop ((l!!i) + 1) l]
 data ComputerState = ComputerState { position :: Int
                                    , tape     :: [Int]
                                    , input    :: [Int]
-                                   , output   :: Int
-                                   }
+                                   , output   :: Maybe Int
+                                   , halted   :: Bool
+                                   } deriving Show
+
+defaultComputerState = ComputerState { position = 0, tape = undefined, input = [], output = Nothing, halted = False }
 
 data ParamMode = Positional | Immediate
 
@@ -29,22 +33,31 @@ toParamMode n magnitudeOfInterest
   where d = mod (div n magnitudeOfInterest) 10
 
 newComputerState :: [Int] -> String -> ComputerState
-newComputerState i ts = ComputerState {position = 0, tape = parseComputerTape ts, input = i, output = -1}
+newComputerState i ts = defaultComputerState { tape = parseComputerTape ts, input = i }
 
 newComputerStateParsedTape :: [Int] -> ComputerState
-newComputerStateParsedTape t = ComputerState {position = 0, tape = t, input = [-1], output = -1}
+newComputerStateParsedTape t = defaultComputerState { tape = t }
 
-compute :: ComputerState -> ComputerState -- TODO: traverse or other recursion scheme?
-compute c@ComputerState{position = i, tape = l, input, output} = case opcode of
-  01 -> compute ComputerState {position = i + 4, tape = replaceAtIndexsValue l (i+3) (firstParam + secondParam), input, output}
-  02 -> compute ComputerState {position = i + 4, tape = replaceAtIndexsValue l (i+3) (firstParam * secondParam), input, output}
-  03 -> compute ComputerState {position = i + 2, tape = replaceAtIndexsValue l (i+1) (head input), input = tail input, output}
-  04 -> compute ComputerState {position = i + 2, tape = l, input, output = firstParam}
-  05 -> compute ComputerState {position = if firstParam /= 0 then secondParam else i + 3, tape = l, input, output}
-  06 -> compute ComputerState {position = if firstParam == 0 then secondParam else i + 3, tape = l, input, output}
-  07 -> compute ComputerState {position = i + 4, tape = replaceAtIndexsValue l (i+3) (if firstParam < secondParam then 1 else 0), input, output}
-  08 -> compute ComputerState {position = i + 4, tape = replaceAtIndexsValue l (i+3) (if firstParam == secondParam then 1 else 0), input, output}
-  99 -> c
+compute :: ComputerState -> ComputerState
+compute = until halted step
+
+nextOutput :: ComputerState -> ComputerState
+nextOutput cs = until (\x -> isJust (output x) || halted x ) step cs { output = Nothing }
+
+appendToInput :: Int -> ComputerState -> ComputerState
+appendToInput n c = c { input = input c ++ [n] }
+
+step :: ComputerState -> ComputerState
+step c@ComputerState{position = i, tape = l, input, output} = case opcode of
+  01 -> c {position = i + 4, tape = replaceAtIndexsValue l (i+3) (firstParam + secondParam)}
+  02 -> c {position = i + 4, tape = replaceAtIndexsValue l (i+3) (firstParam * secondParam)}
+  03 -> c {position = i + 2, tape = replaceAtIndexsValue l (i+1) (head input), input = tail input}
+  04 -> c {position = i + 2, tape = l, input, output = Just firstParam}
+  05 -> c {position = if firstParam /= 0 then secondParam else i + 3, tape = l}
+  06 -> c {position = if firstParam == 0 then secondParam else i + 3, tape = l}
+  07 -> c {position = i + 4, tape = replaceAtIndexsValue l (i+3) (if firstParam < secondParam then 1 else 0)}
+  08 -> c {position = i + 4, tape = replaceAtIndexsValue l (i+3) (if firstParam == secondParam then 1 else 0)}
+  99 -> c { halted = True }
   _  -> error $ "found unexpected instruction: " ++ show (l!!i) ++ " at position " ++ show i
   where
     secondParam = paramModeToAccessor (toParamMode currentVal 1000) (i + 2) l
