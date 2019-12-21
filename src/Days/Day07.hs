df7e17488361d883908fbaa3b500e9a9a82c68fc
Jon Coe <jonmcoe@gmail.com>
2019-12-21 00:29:16 -0500

    day07a done

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -1,9 +1,22 @@
 module Days.Day07 where
 
+import Data.List (maximumBy, permutations)
+import Data.Ord (comparing)
+
 import Days.Common
 
-day07a :: String -> String
-day07a = show . output . compute . newComputerState [1,0]
+getOutputForPair :: (Int, Int) -> String -> Int
+getOutputForPair (a,b) t = output $ compute $ newComputerState [a,b] t
 
+chainCompute :: String -> [Int] -> Int
+chainCompute t = foldl (\prevOutput ampSetting -> getOutputForPair (ampSetting, prevOutput) t) 0
+
+day07a :: String -> String
+day07a t = show $ maximumBy (comparing snd) $ map (\x -> (x, chainCompute t x)) $ permutations [0..4]
+
+
+-- need to further generalize computer to yield FIRST output, not necessarily continue until halt command
+-- i think we also need to maintain the same tape between runs
 day07b :: String -> String
-day07b = show . output . compute . newComputerState [2,0]
+day07b t = show $ maximumBy (comparing snd) $ map (\x -> (x, chainCompute t x)) $ concatMap (generateRepetitions 1) (permutations [5..9])
+  where generateRepetitions n l = map (\x -> concat $ replicate x l) [1..n]
