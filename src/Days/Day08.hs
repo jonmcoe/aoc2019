09c31d8dfe64bea724f41b9b25a9433c4077a904
Jon Coe <jonmcoe@gmail.com>
2019-12-21 22:06:35 -0500

    day08 done

--- src/Days/Day08.hs
+++ src/Days/Day08.hs
@@ -1,5 +1,6 @@
 module Days.Day08 where
 
+import Data.Char
 import Data.List (find, minimumBy, transpose)
 import Data.List.Split
 import Data.Maybe (fromJust)
@@ -11,10 +12,9 @@ height = 6
 productOnesAndTwos l = numChar '1' l * numChar '2' l
 numChar c = length . filter (== c)
 
--- TODO: make formatted layer function
-
 day08a :: String -> String
 day08a = show . productOnesAndTwos . minimumBy (comparing (numChar '0')) . chunksOf (width * height)
 
 day08b :: String -> String
-day08b = show . chunksOf width . map (fromJust . find (/= '2')) . transpose . chunksOf (width * height)
+day08b = unlines . chunksOf width . map (substitute . fromJust . find (/= '2')) . transpose . chunksOf (width * height)
+  where substitute x = " X" !! digitToInt x
