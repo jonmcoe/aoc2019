768eb07d8aa798f29e7858882ef1d3f1e1b32a09
Jon Coe <jonmcoe@gmail.com>
2019-12-24 12:07:32 -0500

    day10b done but it's pretty bad

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -3,14 +3,16 @@
 
 module Days.Day10 where
 
-import Data.List (maximumBy)
+import Data.List (elemIndex, maximumBy, nub)
+import Data.Maybe (fromJust)
 import Data.Ord (comparing)
+import Data.Sort (sort)
 import qualified Data.Set as S
 
-data Orientation = Leftwards | Rightwards deriving (Eq, Ord, Show)
+data Orientation = Rightwards | Leftwards deriving (Eq, Ord, Show)
 
 type Point a = (a, a)
-type Slope a = (Orientation, Maybe a) -- Record might be neater, but we get Ord for free with tuple in this order
+data Slope a = Slope {orientation :: Orientation, direction :: Maybe a} deriving (Eq, Ord, Show)
 
 parse :: (Fractional a, Enum a) => String -> [Point a]
 parse = concat . zipWith (\a b -> map (, a) b) [0..] . map processRowElements . lines
@@ -18,27 +20,31 @@ parse = concat . zipWith (\a b -> map (, a) b) [0..] . map processRowElements . 
     processRowElements row = map fst . filter ((== '#') . snd) $ zip [0..] row
 
 slope :: (Eq a, Fractional a, Ord a) => Point a -> Point a -> Slope a
-slope (x1, y1) (x2, y2) = (orientation, magnitude)
+slope (x1, y1) (x2, y2) = Slope {orientation, direction}
   where
-    magnitude = if x1 == x2 then Nothing else Just ((y2 - y1) / (x2 - x1))
+    direction = if x1 == x2 then Nothing else Just ((y2 - y1) / (x2 - x1)) -- this is messy. y is "downwards" so this SHOULD be reversed, but also we want positives to come before negatives in the ordering, so we are sort of wrong twice here and it works out
     orientation
       | x1 > x2 = Leftwards
       | x1 < x2 = Rightwards
       | y1 > y2 = Leftwards
       | y1 < y2 = Rightwards
 
-pairsWith :: (Eq a, Fractional a) => Int -> [Point a] -> [(Point a, Point  a)]
+pairsWith :: (Eq a, Fractional a) => Int -> [Point a] -> [(Point a, Point a)]
 pairsWith i l = [(l!!i, l!!j) | j <- [0..length l - 1], j /= i]
 
 otherSlopes i l = map (uncurry slope) (pairsWith i l)
 
 numUniqueOtherSlopes :: (Eq a, Fractional a, Ord a) => Int -> [Point a] -> Int
-numUniqueOtherSlopes i l = length $ S.fromList $ otherSlopes i l
+numUniqueOtherSlopes i l = length $ nub $ otherSlopes i l
 
-day10a s = show $ maximum $ map (`numUniqueOtherSlopes` points) [0..length points - 1]
+day10a s = show $ snd $ maximumBy (comparing snd) $ map (\i -> (points!!i, numUniqueOtherSlopes i points)) [0..length points - 1]
   where points = parse s
 
-day10b = show . parse
---  where
---    startingPoint =
---    points        = parse s
+-- one "lap" is sufficient
+day10b s = show candidatesHavingSlope
+  where
+    candidatesHavingSlope = filter (\candidate -> candidate /= startingPoint && slope startingPoint candidate == desiredSlope ) points
+    desiredSlope = sort (nub (otherSlopes startingPointIndex points)) !! 199
+    startingPointIndex = fromJust (elemIndex startingPoint points)
+    startingPoint = fst $ maximumBy (comparing snd) $ map (\i -> (points!!i, numUniqueOtherSlopes i points)) [0..length points - 1]
+    points        = parse s
