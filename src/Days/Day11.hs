aa711293e11fd7a2bc66d6b9dd5f7454d939cca9
Jon Coe <jonmcoe@gmail.com>
2019-12-27 09:03:38 -0500

    day11 done

--- src/Days/Day11.hs
+++ src/Days/Day11.hs
@@ -1,4 +1,40 @@
 module Days.Day11 where
 
-day11a = id
-day11b = id
+import Data.List (minimumBy, maximumBy)
+import qualified Data.Map.Strict as M
+import Data.Ord (comparing)
+
+
+import Days.Common
+
+type Color = Int
+type Point = (Int, Int)
+type PaintedHull = M.Map Point Color
+
+paintedHull :: PaintedHull -> ComputerState -> PaintedHull
+paintedHull acc = paintedHullImpl acc (0,1) (0,0)
+
+paintedHullImpl :: PaintedHull -> Point -> Point -> ComputerState -> PaintedHull
+paintedHullImpl acc bearing loc cs = case output nextCs of
+  Just o   -> paintedHullImpl (M.insert loc o acc) nextBearing nextLoc nextNextCs
+  Nothing -> acc
+  where
+    nextLoc = (fst loc + fst nextBearing, snd loc + snd nextBearing)
+    nextBearing = case output nextNextCs of -- 0: left, 1: right
+      Just 0 -> (snd bearing * (-1), fst bearing)
+      Just 1 -> (snd bearing, fst bearing * (-1))
+    nextNextCs = untilOutput nextCs
+    nextCs = untilOutput (appendToInput curColor cs)
+    curColor = M.findWithDefault 0 loc acc
+
+showPaintedHull ph = unlines $ map makeRow [maxY,maxY-1..minY]
+  where
+    makeRow y = map sqDisplay [(xx, y) | xx <- [minX..maxX]] ++ "\n"
+    sqDisplay cell = if M.findWithDefault 0 cell ph == 1 then '#' else ' '
+    maxY = snd $ maximumBy (comparing snd) $ M.keys ph
+    minY = snd $ minimumBy (comparing snd) $ M.keys ph
+    maxX = fst $ maximumBy (comparing fst) $ M.keys ph
+    minX = fst $ minimumBy (comparing fst) $ M.keys ph
+
+day11a = show . length . M.keys . paintedHull M.empty . newComputerState []
+day11b = showPaintedHull . paintedHull (M.singleton (0,0) 1) . newComputerState []
